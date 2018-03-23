a2f9af1eaa165bdcfa1f1717cd5c81eac0b4095a
myapps-solutions <35138605+myapps-solutions@users.noreply.github.com>
2018-03-23 16:12:46 +0530

    file edited
    test

--- Huddil_UI build/src/app/provider/meetup.service.ts
+++ Huddil_UI build/src/app/provider/meetup.service.ts
@@ -142,7 +142,7 @@ export class MeetupService {
 
         let protocol = location.protocol;
         //this.baseUrl = protocol + '//localhost:4200/';
-        this.baseUrl = protocol + '//198.1.85.118/';
+        this.baseUrl = protocol + '//ec2-13-127-102-181.ap-south-1.compute.amazonaws.com//';
         //this.baseUrl = protocol + '//huddil.com/';
 
         this.originurl = (platformLocation as any).location.origin;
